815168dab0b7797688e4686ba7de0d3d84cbe852
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-05-21 11:50:04 -0300

    feat: botão adicionar colocou os valores dentro do campo select, através do appendChild

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let num = document.getElementById('fnum')
 let btnAdc = document.getElementById('btn1')
+let lista = document.getElementById('flista')
+let valores = []
 
 btnAdc.addEventListener('click', () => {
-    if (num.value == "" ) {
+    if (Number(num.value) == "" ) {
         alert('Insira um numero')
+        num.value = ""
         return
-    } if (num.value < 1 || num.value > 100) {
+    } if (Number(num.value) < 1 || Number(num.value > 100)) {
         alert('Adicione um número válido')
-    }    
+        num.value = ""
+        return
+    } if (Number(num.value) >= 1 && Number(num.value <= 100)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        num.value = ""
+    }   
 })
